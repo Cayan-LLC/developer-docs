e926aa375e4066557c9cab406b8b391580a12447
rdriscoll-cayan <rdriscoll@cayan.com>
2018-04-12 14:53:39 -0400

    Initial Commit of UpdateBoardedCard

---
+++ examples/Merchantware/Credit/Vb/UpdateBoardedCard/UpdateBoardedCard/Module1.vb
@@ -0,0 +1,32 @@
+﻿Imports UpdateBoardedCard.MWCredit
+
+Module Module1
+
+    Sub Main()
+        'Create Soap Client
+        Dim MWCredit As New CreditSoapClient
+
+        'Create Credentails Object
+        Dim Credentials As New MerchantCredentials With {
+        .MerchantName = "TEST MERCHANT",
+        .MerchantSiteId = "XXXXXXXX",
+        .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
+        }
+
+        'Create Request Object
+        Dim Request As New UpdateBoardedCardRequest With {
+        .VaultToken = "100000100ABCDE123456",
+        .ExpirationDate = "1221"
+        }
+
+        'Process Request
+        Dim Response As VaultBoardingResponse45
+        Response = MWCredit.UpdateBoardedCard(Credentials, Request)
+
+        'Display Results
+        Console.WriteLine(" Vault Token: {0} Error Code: {1} Error Message: {2}", Response.VaultToken + vbNewLine, Response.ErrorCode + vbNewLine, Response.ErrorMessage + vbNewLine)
+        Console.WriteLine("Press Any Key to Close")
+        Console.ReadKey()
+    End Sub
+
+End Module
