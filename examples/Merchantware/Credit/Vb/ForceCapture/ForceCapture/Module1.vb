0b4a81d24fc79295aa9406e732ee4e068852407b
rdriscoll-cayan <rdriscoll@cayan.com>
2018-04-17 14:30:21 -0400

    Formatting Corrections

--- examples/Merchantware/Credit/Vb/ForceCapture/ForceCapture/Module1.vb
+++ examples/Merchantware/Credit/Vb/ForceCapture/ForceCapture/Module1.vb
@@ -3,39 +3,35 @@
 Module Module1
 
     Sub Main()
+
         'Create Soap Client
-        Dim MWCredit As New CreditSoapClient
-
+        Dim CreditSoapClient As New CreditSoapClient
         'Create Credentials Object
-        Dim Credentials As New MerchantCredentials With {
-        .MerchantName = "TEST MERCHANT",
-        .MerchantSiteId = "XXXXXXXX",
-        .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
-        }
-
+        Dim merchantCredentials As New MerchantCredentials With {
+            .MerchantName = "TEST MERCHANT",
+            .MerchantSiteId = "XXXXXXXX",
+            .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
+            }
         'Create PaymentData Object
         Dim PaymentData As New PaymentData With {
-        .Source = "KEYED",
-        .CardNumber = "4012000033330026",
-        .ExpirationDate = "1221",
-        .CardHolder = "John Doe"
-        }
-
+            .Source = "KEYED",
+            .CardNumber = "4012000033330026",
+            .ExpirationDate = "1221",
+            .CardHolder = "John Doe"
+            }
         'Create Request Object
-        Dim ForceRequest As New ForceCaptureRequest With {
-        .Amount = "1.01",
-        .AuthorizationCode = "ABC123",
-        .InvoiceNumber = "INV1234",
-        .RegisterNumber = "01",
-        .CardAcceptorTerminalId = "01"
-        }
-
+        Dim ForceCaptureRequest As New ForceCaptureRequest With {
+            .Amount = "1.01",
+            .AuthorizationCode = "ABC123",
+            .InvoiceNumber = "INV1234",
+            .RegisterNumber = "01",
+            .CardAcceptorTerminalId = "01"
+            }
         'Process Request
-        Dim ForceResponse As TransactionResponse45
-        ForceResponse = MWCredit.ForceCapture(Credentials, PaymentData, ForceRequest)
-
+        Dim TransactionResponse45 As TransactionResponse45
+        TransactionResponse45 = CreditSoapClient.ForceCapture(merchantCredentials, PaymentData, ForceCaptureRequest)
         'Display Results
-        Console.WriteLine(" Force Capture Response: {0} Token: {1} Amount: ${2} RFMIQ: {3}", ForceResponse.ApprovalStatus + vbNewLine, ForceResponse.Token + vbNewLine, ForceResponse.Amount + vbNewLine, ForceResponse.Rfmiq + vbNewLine)
+        Console.WriteLine(" Force Capture Response: {0} Token: {1} Amount: ${2} RFMIQ: {3}", TransactionResponse45.ApprovalStatus + vbNewLine, TransactionResponse45.Token + vbNewLine, TransactionResponse45.Amount + vbNewLine, TransactionResponse45.Rfmiq + vbNewLine)
         Console.WriteLine("Press Any Key to Close")
         Console.ReadKey()
     End Sub
