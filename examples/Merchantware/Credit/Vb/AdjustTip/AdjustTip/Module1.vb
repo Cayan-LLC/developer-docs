3ad9f6eafcbb15b1dd8964cc284bd27d9132c378
rdriscoll-cayan <rdriscoll@cayan.com>
2018-04-17 15:01:47 -0400

    Formatting Corrections

--- examples/Merchantware/Credit/Vb/AdjustTip/AdjustTip/Module1.vb
+++ examples/Merchantware/Credit/Vb/AdjustTip/AdjustTip/Module1.vb
@@ -3,28 +3,25 @@
 Module Module1
 
     Sub Main()
+
         'Create Soap Client
-        Dim MWCredit As New CreditSoapClient
-
+        Dim creditSoapClient As New CreditSoapClient
         'Create Credentials Object
-        Dim Credentials As New MerchantCredentials With {
-        .MerchantName = "TEST MERCHANT",
-        .MerchantSiteId = "XXXXXXXX",
-        .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
-        }
-
+        Dim merchantCredentials As New MerchantCredentials With {
+            .MerchantName = "TEST MERCHANT",
+            .MerchantSiteId = "XXXXXXXX",
+            .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
+            }
         'Create Request Object
-        Dim AdjustRequest As New TipRequest With {
-        .Token = "1234567890",
-        .Amount = "1.01"
-        }
-
+        Dim tipRequest As New TipRequest With {
+            .Token = "1234567890",
+            .Amount = "1.01"
+            }
         'Process Request
-        Dim AdjustResponse As TransactionResponse45
-        AdjustResponse = MWCredit.AdjustTip(Credentials, AdjustRequest)
-
+        Dim transactionResponse45 As TransactionResponse45
+        transactionResponse45 = creditSoapClient.AdjustTip(merchantCredentials, tipRequest)
         'Display Results
-        Console.WriteLine(" Adjust Response: {0} Token: {1} Amount: ${2} RFMIQ: {3}", AdjustResponse.ApprovalStatus + vbNewLine, AdjustResponse.Token + vbNewLine, AdjustResponse.Amount + vbNewLine, AdjustResponse.Rfmiq + vbNewLine)
+        Console.WriteLine(" Adjust Response: {0} Token: {1} Authorization Code: {2} Transaction Date: {3} Amount: ${4} Remaining Card Balance: {5} Card Number: {6} Cardholder: {7} Card Type: {8} FSA Card: {9} Reader Entry Mode: {10} AVS Response: {11} CV Response: {12} Error Message: {13} Extra Data: {14}", transactionResponse45.ApprovalStatus + vbNewLine, transactionResponse45.Token + vbNewLine, transactionResponse45.AuthorizationCode + vbNewLine, transactionResponse45.TransactionDate + vbNewLine, transactionResponse45.Amount + vbNewLine, transactionResponse45.RemainingCardBalance + vbNewLine, transactionResponse45.CardNumber + vbNewLine, transactionResponse45.Cardholder + vbNewLine, transactionResponse45.CardType + vbNewLine, transactionResponse45.FsaCard + vbNewLine, transactionResponse45.ReaderEntryMode + vbNewLine, transactionResponse45.AvsResponse + vbNewLine, transactionResponse45.CvResponse + vbNewLine, transactionResponse45.ErrorMessage + vbNewLine, transactionResponse45.ExtraData + vbNewLine)
         Console.WriteLine("Press Any Key to Close")
         Console.ReadKey()
     End Sub
