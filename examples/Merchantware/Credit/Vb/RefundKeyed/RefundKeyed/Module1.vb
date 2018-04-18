519b42c767b3c93a0e2c177dd42311b7c009cfaa
rdriscoll-cayan <rdriscoll@cayan.com>
2018-04-18 12:54:53 -0400

    Format Changes

--- examples/Merchantware/Credit/Vb/RefundKeyed/RefundKeyed/Module1.vb
+++ examples/Merchantware/Credit/Vb/RefundKeyed/RefundKeyed/Module1.vb
@@ -1,42 +1,37 @@
-﻿Imports RefundKeyed.MWCredit
+﻿Imports RefundKeyed.MWCredit45
 
 Module Module1
 
     Sub Main()
         'Create Soap Client
-        Dim MWCredit As New CreditSoapClient
-
+        Dim creditSoapClient As New CreditSoapClient
         'Create Credentails Object
-        Dim Credentials As New MerchantCredentials With {
-        .MerchantName = "TEST MERCHANT",
-        .MerchantSiteId = "XXXXXXXX",
-        .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
+        Dim merchantCredentials As New MerchantCredentials With {
+            .MerchantName = "TEST MERCHANT",
+            .MerchantSiteId = "XXXXXXXX",
+            .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
         }
-
         'Create PaymentData Object
-        Dim PaymentData As New PaymentData With {
-        .Source = "KEYED",
-        .CardNumber = "4012000033330026",
-        .ExpirationDate = "1221",
-        .CardHolder = "John Doe",
-        .AvsStreetAddress = "1 Federal St",
-        .AvsZipCode = "02110",
-        .CardVerificationValue = "123"
+        Dim paymentData As New PaymentData With {
+            .Source = "KEYED",
+            .CardNumber = "4012000033330026",
+            .ExpirationDate = "1221",
+            .CardHolder = "John Doe",
+            .AvsStreetAddress = "1 Federal St",
+            .AvsZipCode = "02110",
+            .CardVerificationValue = "123"
         }
-
         'Create Request Object
-        Dim RefundRequest As New RefundRequest With {
-        .Amount = "1.01",
-        .InvoiceNumber = "INV1234",
-        .CardAcceptorTerminalId = "01"
+        Dim refundRequest As New RefundRequest With {
+            .Amount = "1.01",
+            .InvoiceNumber = "INV1234",
+            .CardAcceptorTerminalId = "01"
         }
-
         'Process Request
-        Dim RefundResponse As TransactionResponse45
-        RefundResponse = MWCredit.Refund(Credentials, PaymentData, RefundRequest)
-
+        Dim transactionResponse45 As TransactionResponse45
+        transactionResponse45 = creditSoapClient.Refund(merchantCredentials, paymentData, refundRequest)
         'Display Results
-        Console.WriteLine(" Refund Response: {0} Token: {1} Amount: ${2} RFMIQ: {3}", RefundResponse.ApprovalStatus + vbNewLine, RefundResponse.Token + vbNewLine, RefundResponse.Amount + vbNewLine, RefundResponse.Rfmiq + vbNewLine)
+        Console.WriteLine(" Refund Response: {0} Token: {1} Amount: ${2}", transactionResponse45.ApprovalStatus + vbNewLine, transactionResponse45.Token + vbNewLine, transactionResponse45.Amount + vbNewLine)
         Console.WriteLine("Press Any Key to Close")
         Console.ReadKey()
     End Sub
