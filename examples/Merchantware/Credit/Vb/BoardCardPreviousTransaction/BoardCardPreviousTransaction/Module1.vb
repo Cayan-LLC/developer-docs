58be77af6c686f342850538b03d5ec0d53849f1b
rdriscoll-cayan <rdriscoll@cayan.com>
2018-04-18 12:27:25 -0400

    Format Changes

--- examples/Merchantware/Credit/Vb/BoardCardPreviousTransaction/BoardCardPreviousTransaction/Module1.vb
+++ examples/Merchantware/Credit/Vb/BoardCardPreviousTransaction/BoardCardPreviousTransaction/Module1.vb
@@ -1,33 +1,28 @@
-﻿Imports BoardCardPreviousTransaction.MWCredit
+﻿Imports BoardCardPreviousTransaction.MWCredit45
 
 Module Module1
 
     Sub Main()
         'Create Soap Client
-        Dim MWCredit As New CreditSoapClient
-
+        Dim creditSoapClient As New CreditSoapClient
         'Create Credentials Object
-        Dim Credentials As New MerchantCredentials With {
-        .MerchantName = "TEST MERCHANT",
-        .MerchantSiteId = "XXXXXXXX",
-        .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
+        Dim merchantCredentials As New MerchantCredentials With {
+            .MerchantName = "TEST MERCHANT",
+            .MerchantSiteId = "XXXXXXXX",
+            .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
         }
-
         'Create PaymentData Object
-        Dim PaymentData As New PaymentData With {
-        .Source = "PREVIOUSTRANSACTION",
-        .Token = "1234567890"
+        Dim paymentData As New PaymentData With {
+            .Source = "PREVIOUSTRANSACTION",
+            .Token = "1234567890"
         }
-
         'Create Request Object
-        Dim BoardingRequest As New BoardingRequest
-
+        Dim boardingRequest As New BoardingRequest
         'Process Request
-        Dim BoardingResponse As VaultBoardingResponse45
-        BoardingResponse = MWCredit.BoardCard(Credentials, PaymentData, BoardingRequest)
-
+        Dim vaultBoardingResponse45 As VaultBoardingResponse45
+        vaultBoardingResponse45 = creditSoapClient.BoardCard(merchantCredentials, paymentData, boardingRequest)
         'Display Results
-        Console.WriteLine(" Vault Token: {0} Error Code: {1} Error Message: {2} RFMIQ: {3}", BoardingResponse.VaultToken + vbNewLine, BoardingResponse.ErrorCode + vbNewLine, BoardingResponse.ErrorMessage + vbNewLine, BoardingResponse.Rfmiq + vbNewLine)
+        Console.WriteLine(" Vault Token: {0} Error Code: {1} Error Message: {2}", vaultBoardingResponse45.VaultToken + vbNewLine, vaultBoardingResponse45.ErrorCode + vbNewLine, vaultBoardingResponse45.ErrorMessage + vbNewLine)
         Console.WriteLine("Press Any Key to Close")
         Console.ReadKey()
     End Sub
