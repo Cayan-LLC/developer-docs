69942555b82d6bc9692cffffe1f264e2a3c37214
rdriscoll-cayan <rdriscoll@cayan.com>
2018-04-18 12:58:13 -0400

    Format Changes

--- examples/Merchantware/Credit/Vb/RefundPreviousTransaction/RefundPreviousTransaction/Module1.vb
+++ examples/Merchantware/Credit/Vb/RefundPreviousTransaction/RefundPreviousTransaction/Module1.vb
@@ -1,37 +1,32 @@
-﻿Imports RefundPreviousTransaction.MWCredit
+﻿Imports RefundPreviousTransaction.MWCredit45
 
 Module Module1
 
     Sub Main()
         'Create Soap Client
-        Dim MWCredit As New CreditSoapClient
-
+        Dim creditSoapClient As New CreditSoapClient
         'Create Credentials Object
-        Dim Credentials As New MerchantCredentials With {
-        .MerchantName = "TEST MERCHANT",
-        .MerchantSiteId = "XXXXXXXX",
-        .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
+        Dim merchantCredentials As New MerchantCredentials With {
+            .MerchantName = "TEST MERCHANT",
+            .MerchantSiteId = "XXXXXXXX",
+            .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
         }
-
         'Create PaymentData Object
-        Dim PaymentData As New PaymentData With {
-        .Source = "PREVIOUSTRANSACTION",
-        .Token = "1234567890"
+        Dim paymentData As New PaymentData With {
+            .Source = "PREVIOUSTRANSACTION",
+            .Token = "1234567890"
         }
-
         'Create Request Object
-        Dim RefundRequest As New RefundRequest With {
-        .Amount = "1.01",
-        .InvoiceNumber = "INV1234",
-        .CardAcceptorTerminalId = "01"
+        Dim refundRequest As New RefundRequest With {
+            .Amount = "1.01",
+            .InvoiceNumber = "INV1234",
+            .CardAcceptorTerminalId = "01"
         }
-
         'Process Request
-        Dim RefundResponse As TransactionResponse45
-        RefundResponse = MWCredit.Refund(Credentials, PaymentData, RefundRequest)
-
+        Dim transactionResponse45 As TransactionResponse45
+        transactionResponse45 = creditSoapClient.Refund(merchantCredentials, paymentData, refundRequest)
         'Display Results
-        Console.WriteLine(" Refund Response: {0} Token: {1} Amount: ${2} RFMIQ: {3}", RefundResponse.ApprovalStatus + vbNewLine, RefundResponse.Token + vbNewLine, RefundResponse.Amount + vbNewLine, RefundResponse.Rfmiq + vbNewLine)
+        Console.WriteLine(" Refund Response: {0} Token: {1} Amount: ${2}", transactionResponse45.ApprovalStatus + vbNewLine, transactionResponse45.Token + vbNewLine, transactionResponse45.Amount + vbNewLine)
         Console.WriteLine("Press Any Key to Close")
         Console.ReadKey()
     End Sub
