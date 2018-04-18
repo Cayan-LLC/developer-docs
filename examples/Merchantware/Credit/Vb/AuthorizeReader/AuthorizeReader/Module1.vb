431ff12dcd5e2470b9ac18cf6f302f99f0b27684
rdriscoll-cayan <rdriscoll@cayan.com>
2018-04-18 11:48:10 -0400

    Format Changes

--- examples/Merchantware/Credit/Vb/AuthorizeReader/AuthorizeReader/Module1.vb
+++ examples/Merchantware/Credit/Vb/AuthorizeReader/AuthorizeReader/Module1.vb
@@ -1,41 +1,36 @@
-﻿Imports AuthorizeReader.MWCredit
+﻿Imports AuthorizeReader.MWCredit45
 
 Module Module1
 
     Sub Main()
         'Create Soap Client
-        Dim MWCredit As New CreditSoapClient
-
+        Dim creditSoapClient As New CreditSoapClient
         'Create Credentials Object
-        Dim Credentials As New MerchantCredentials With {
-        .MerchantName = "TEST MERCHANT",
-        .MerchantSiteId = "XXXXXXXX",
-        .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
+        Dim merchantCredentials As New MerchantCredentials With {
+            .MerchantName = "TEST MERCHANT",
+            .MerchantSiteId = "XXXXXXXX",
+            .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
         }
-
         'Create PaymentData Object
-        Dim PaymentData As New PaymentData With {
-        .Source = "READER",
-        .TrackData = "%B4012000033330026^TEST CARD/GENIUS^191210054321000000000000000 150;4012000033330026=19121011000012345678?"
+        Dim paymentData As New PaymentData With {
+            .Source = "READER",
+            .TrackData = "%B4012000033330026^TEST CARD/GENIUS^191210054321000000000000000 150;4012000033330026=19121011000012345678?"
         }
-
         'Create Request Object
-        Dim AuthorizeRequest As New AuthorizationRequest With {
-        .Amount = "1.01",
-        .TaxAmount = "0.10",
-        .InvoiceNumber = "INV1234",
-        .CardAcceptorTerminalId = "01",
-        .CustomerCode = "1234",
-        .PurchaseOrderNumber = "PO1234",
-        .EnablePartialAuthorization = "true"
+        Dim authorizationRequest As New AuthorizationRequest With {
+            .Amount = "1.01",
+            .TaxAmount = "0.10",
+            .InvoiceNumber = "INV1234",
+            .CardAcceptorTerminalId = "01",
+            .CustomerCode = "1234",
+            .PurchaseOrderNumber = "PO1234",
+            .EnablePartialAuthorization = "true"
         }
-
         'Process Request
-        Dim AuthorizeResponse As TransactionResponse45
-        AuthorizeResponse = MWCredit.Authorize(Credentials, PaymentData, AuthorizeRequest)
-
+        Dim transactionResponse45 As TransactionResponse45
+        transactionResponse45 = creditSoapClient.Authorize(merchantCredentials, paymentData, authorizationRequest)
         'Display Results
-        Console.WriteLine(" Authorization Response: {0} Token: {1} Amount: ${2} RFMIQ: {3}", AuthorizeResponse.ApprovalStatus + vbNewLine, AuthorizeResponse.Token + vbNewLine, AuthorizeResponse.Amount + vbNewLine, AuthorizeResponse.Rfmiq + vbNewLine)
+        Console.WriteLine(" Authorization Response: {0} Token: {1} Amount: ${2}", transactionResponse45.ApprovalStatus + vbNewLine, transactionResponse45.Token + vbNewLine, transactionResponse45.Amount + vbNewLine)
         Console.WriteLine("Press Any Key to Close")
         Console.ReadKey()
     End Sub
