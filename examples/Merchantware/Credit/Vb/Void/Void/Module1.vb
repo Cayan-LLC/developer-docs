028c87cea13635d24586d76b9817718296df2d97
rdriscoll-cayan <rdriscoll@cayan.com>
2018-04-19 11:53:58 -0400

    Format Changes

--- examples/Merchantware/Credit/Vb/Void/Void/Module1.vb
+++ examples/Merchantware/Credit/Vb/Void/Void/Module1.vb
@@ -1,30 +1,26 @@
-﻿Imports Void.MWCredit
+﻿Imports Void.MWCredit45
 
 Module Module1
 
     Sub Main()
         'Create Soap Client
-        Dim MWCredit As New CreditSoapClient
-
+        Dim creditSoapClient As New CreditSoapClient
         'Create Credentails Object
-        Dim Credentials As New MerchantCredentials With {
-        .MerchantName = "TEST MERCHANT",
-        .MerchantSiteId = "XXXXXXXX",
-        .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
+        Dim merchantCredentials As New MerchantCredentials With {
+            .MerchantName = "TEST MERCHANT",
+            .MerchantSiteId = "XXXXXXXX",
+            .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
         }
-
         'Create Request Object
-        Dim VoidRequest As New VoidRequest With {
-        .Token = "1234567890",
-        .CardAcceptorTerminalId = "01"
+        Dim voidRequest As New VoidRequest With {
+            .Token = "1234567890",
+            .CardAcceptorTerminalId = "01"
         }
-
         'Process Request
-        Dim VoidResponse As TransactionResponse45
-        VoidResponse = MWCredit.Void(Credentials, VoidRequest)
-
+        Dim transactionResponse45 As TransactionResponse45
+        transactionResponse45 = creditSoapClient.Void(merchantCredentials, voidRequest)
         'Display Results
-        Console.WriteLine(" Void Response: {0} Token: {1} Amount: ${2} RFMIQ: {3}", VoidResponse.ApprovalStatus + vbNewLine, VoidResponse.Token + vbNewLine, VoidResponse.Amount + vbNewLine, VoidResponse.Rfmiq + vbNewLine)
+        Console.WriteLine(" Void Response: {0} Token: {1} Amount: ${2}", transactionResponse45.ApprovalStatus + vbNewLine, transactionResponse45.Token + vbNewLine, transactionResponse45.Amount + vbNewLine)
         Console.WriteLine("Press Any Key to Close")
         Console.ReadKey()
     End Sub
