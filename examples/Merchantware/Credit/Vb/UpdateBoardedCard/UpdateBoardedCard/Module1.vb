1a23b661f8892be09f98e24fc3746d80675ae162
rdriscoll-cayan <rdriscoll@cayan.com>
2018-04-19 11:52:36 -0400

    Format Changes

--- examples/Merchantware/Credit/Vb/UpdateBoardedCard/UpdateBoardedCard/Module1.vb
+++ examples/Merchantware/Credit/Vb/UpdateBoardedCard/UpdateBoardedCard/Module1.vb
@@ -1,30 +1,26 @@
-﻿Imports UpdateBoardedCard.MWCredit
+﻿Imports UpdateBoardedCard.MWCredit45
 
 Module Module1
 
     Sub Main()
         'Create Soap Client
-        Dim MWCredit As New CreditSoapClient
-
+        Dim creditSoapClient As New CreditSoapClient
         'Create Credentails Object
-        Dim Credentials As New MerchantCredentials With {
-        .MerchantName = "TEST MERCHANT",
-        .MerchantSiteId = "XXXXXXXX",
-        .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
+        Dim merchantCredentials As New MerchantCredentials With {
+            .MerchantName = "TEST MERCHANT",
+            .MerchantSiteId = "XXXXXXXX",
+            .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
         }
-
         'Create Request Object
-        Dim Request As New UpdateBoardedCardRequest With {
-        .VaultToken = "100000100ABCDE123456",
-        .ExpirationDate = "1221"
+        Dim updateBoardedCardRequest As New UpdateBoardedCardRequest With {
+            .VaultToken = "100000100ABCDE123456",
+            .ExpirationDate = "1221"
         }
-
         'Process Request
-        Dim Response As VaultBoardingResponse45
-        Response = MWCredit.UpdateBoardedCard(Credentials, Request)
-
+        Dim vaultBoardingResponse45 As VaultBoardingResponse45
+        vaultBoardingResponse45 = creditSoapClient.UpdateBoardedCard(merchantCredentials, updateBoardedCardRequest)
         'Display Results
-        Console.WriteLine(" Vault Token: {0} Error Code: {1} Error Message: {2}", Response.VaultToken + vbNewLine, Response.ErrorCode + vbNewLine, Response.ErrorMessage + vbNewLine)
+        Console.WriteLine(" Vault Token: {0} Error Code: {1} Error Message: {2}", vaultBoardingResponse45.VaultToken + vbNewLine, vaultBoardingResponse45.ErrorCode + vbNewLine, vaultBoardingResponse45.ErrorMessage + vbNewLine)
         Console.WriteLine("Press Any Key to Close")
         Console.ReadKey()
     End Sub
